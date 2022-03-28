81ccd10bbb9f456830aa93da9207a7be0977f37f
Teresa Campbell <teresa.j.campbell@gmail.com>
2022-03-28 16:34:43 -0400

    fixed server.js mongo address

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+
 const mongoose = require('mongoose');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,14 +11,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true 
 });
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
-
-app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
